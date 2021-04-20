a2aa8568013b1ad23a0d7c037cb3752878014325
BecaLParker <becaparker@gmail.com>
2021-04-20 11:25:01 +0100

    Passes the 0 test case

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,13 @@
 def fizzbuzz(number)
-  if number % 5 == 0 && number % 3 == 0
-    return 'fizzbuzz'
+  if number == 0
+    number
+  elsif number % 5 == 0 && number % 3 == 0
+    'fizzbuzz'
   elsif number % 3 == 0
-    return 'fizz'
+    'fizz'
   elsif number % 5 == 0
-    return 'buzz'
+    'buzz'
   else
     number
-  end
+    end
 end
